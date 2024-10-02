d74a0c34eabb7e3b698989cb35c6878dda8cdeb1
Swapnil Barsagade <94060218+Swapnilbarsagade@users.noreply.github.com>
2024-10-02 16:56:46 +0530

    file updatedto deploy

--- Docker/frontend/worker.service.ts
+++ Docker/frontend/worker.service.ts
@@ -9,7 +9,7 @@ import { Worker } from '../models/worker';
 })
 export class WorkerService {
 
-  private getUrl: string = "http://54.235.63.202:8085/api/v1/workers";
+  private getUrl: string = "http://15.168.37.167:8085/api/v1/workers";
 
   constructor(private _httpClient: HttpClient) { }
 
